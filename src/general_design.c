663b2043ea65f4ec84ebf87956b94d4cafa8a7dd
sangameshpatlolla <sangameshwar1221@gmail.com>
2020-03-24 20:25:53 -0400

    implemented loading bar and welcome screen

--- src/general_design.c
+++ src/general_design.c
@@ -46,6 +46,118 @@ void ccolor(int clr){
 
 }
 
+void pwellcome(){
+	ccolor(26);
+
+	char welcome[50]="WELCOME";
+	char welcome2[50]=" TO";
+	char welcome3[50]=" FOOD ORDERING";
+	char welcome4[50]=" KIOSK MACHINE";
+	printf("\n\n\n\n\n\t\t\t");
+	for(int wlc=0; wlc<strlen(welcome);wlc++){
+             ccolor(120+(wlc*9));
+
+		printf(" %c",welcome[wlc]);
+		Sleep(200);
+	}
+	ccolor(26);
+	printf("\n\n\t\t\t\t ");
+	for(int wlc2=0; wlc2<strlen(welcome2) ;wlc2++){
+            ccolor(160+(wlc2*9));
+
+		printf(" %c",welcome2[wlc2]);
+		Sleep(200);
+	}
+	ccolor(26);
+	printf("\n\n\n\t\t\t ");
+	for(int wlc3=0; wlc3<strlen(welcome3) ;wlc3++){
+		if(welcome3[wlc3]!='D'){
+		    ccolor(121+(wlc3*4));
+
+			printf(" %c",welcome3[wlc3]);
+		}
+		else{
+		    ccolor(11);
+
+			printf(" %c",welcome3[wlc3]);
+		}
+
+		Sleep(200);
+	}
+	ccolor(26);
+	printf("\n\n\n\t\t\t\t ");
+	for(int wlc3=0; wlc3<strlen(welcome4) ;wlc3++){
+		if(welcome4[wlc3]!='A' && welcome4[wlc3]!='E'){
+                ccolor(121+(wlc3*4));
+
+			printf(" %c",welcome4[wlc3]);
+		}
+		else{
+		    ccolor(11);
+
+			printf(" %c",welcome4[wlc3]);
+		}
+		Sleep(200);
+	}
+	ccolor(26);
+
+}
+void loadingbar(void){
+
+	for (int i=15;i<=100;i+=5){
+
+		cls();
+		ccolor(26);
+
+		printf("\n\n\n\n\n\n\n\t\t\t\t");
+		printf("%d %% Loading...\n\n\t\t",i);
+
+		printf("");
+
+		for (int j=0; j<i;j+=2){
+
+			ccolor(160+j);
+			printf(" ");
+			ccolor(26);
+
+		}
+		Sleep(100);
+		if(i==90 || i==50 || i==96 || i==83){
+			Sleep(100);
+		}
+
+	}
+
+}
+void backuploader(void){
+
+	for (int i=15;i<=100;i+=5){
+
+		cls();
+		ccolor(26);
+
+		printf("\n\n\n\n\n\n\n\t\t\t\t");
+		printf("%d %% Backing UP DATA...\n\n\t\t",i);
+
+		printf("");
+
+		for (int j=0; j<i;j+=2){
+
+			ccolor(120+j);
+			printf(" ");
+			ccolor(26);
+
+		}
+		Sleep(50);
+		if(i==90 || i==50 || i==96 || i==83){
+			Sleep(50);
+		}
+	}
+
+}
+
+
+
 void middle1(void){
 
 	printf("\n\n\n\n\n\n\n");
