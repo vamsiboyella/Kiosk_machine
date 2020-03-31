c780727b3d9cdef809589f4a0fce3b0f2ad94a74
matheen5757 <matheen5757@gmail.com>
2020-03-31 17:44:27 -0400

    Doxygen comments for the admin header file

--- include/general_design.h
+++ include/general_design.h
@@ -1,13 +1,129 @@
+/**
+ * @file general_design.h
+ *
+ * @brief This is the main file of design module in system from which all the funtions related to design will be handled.
+ *
+ * @author 
+ * 
+ */
 
+/**
+ * @brief A Function to store the details of patients in "patient_record.csv".
+ * 
+ * @details Function "cls" . 
+ * Invokes disease function which identifies symptom and allocates room accordingly based on availability.
+ * 
+ * @return Return value is void.
+ *
+ */ 
 void cls();
+
+/**
+ * @brief A Function to store the details of patients in "patient_record.csv".
+ * 
+ * @details Function "echo" . 
+ * Invokes disease function which identifies symptom and allocates room accordingly based on availability.
+ * 
+ * @return Return value is void.
+ *
+ */ 
 void echo(char print[]);
+
+/**
+ * @brief A Function to store the details of patients in "patient_record.csv".
+ * 
+ * @details Function "br". 
+ * Invokes disease function which identifies symptom and allocates room accordingly based on availability.
+ * 
+ * @return Return value is void.
+ *
+ */ 
 void br(int line);
+
+/**
+ * @brief A Function to store the details of patients in "patient_record.csv".
+ * 
+ * @details Function "pre" . 
+ * Invokes disease function which identifies symptom and allocates room accordingly based on availability.
+ * 
+ * @return Return value is void.
+ *
+ */ 
 void pre(int tab);
+
+/**
+ * @brief A Function to store the details of patients in "patient_record.csv".
+ * 
+ * @details Function "span". 
+ * Invokes disease function which identifies symptom and allocates room accordingly based on availability.
+ * 
+ * @return Return value is void.
+ *
+ */ 
 void span(int space);
+
+/**
+ * @brief A Function to store the details of patients in "patient_record.csv".
+ * 
+ * @details Function "clr" . 
+ * Invokes disease function which identifies symptom and allocates room accordingly based on availability.
+ * 
+ * @return Return value is void.
+ *
+ */ 
 void ccolor(int clr);
+
+/**
+ * @brief A Function to store the details of patients in "patient_record.csv".
+ * 
+ * @details Function "pwellcome" . 
+ * Invokes disease function which identifies symptom and allocates room accordingly based on availability.
+ * 
+ * @return Return value is void.
+ *
+ */ 
 void pwellcome();
+
+/**
+ * @brief A Function to store the details of patients in "patient_record.csv".
+ * 
+ * @details Function "loadingbar" . 
+ * Invokes disease function which identifies symptom and allocates room accordingly based on availability.
+ * 
+ * @return Return value is void.
+ *
+ */ 
 void loadingbar();
+
+/**
+ * @brief A Function to store the details of patients in "patient_record.csv".
+ * 
+ * @details Function "middle1" . 
+ * Invokes disease function which identifies symptom and allocates room accordingly based on availability.
+ * 
+ * @return Return value is void.
+ *
+ */ 
 void middle1(void);
+
+/**
+ * @brief A Function to store the details of patients in "patient_record.csv".
+ * 
+ * @details Function "middtab1" . 
+ * Invokes disease function which identifies symptom and allocates room accordingly based on availability.
+ * 
+ * @return Return value is void.
+ *
+ */ 
 void middtab1(void);
+
+/**
+ * @brief A Function to store the details of patients in "patient_record.csv".
+ * 
+ * @details Function "backuploader" . 
+ * Invokes disease function which identifies symptom and allocates room accordingly based on availability.
+ * 
+ * @return Return value is void.
+ *
+ */ 
 void backuploader(void);
-void main_menu();
