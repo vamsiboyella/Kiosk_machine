2ca5fee842da059e878cb4809213353117a1fcd6
vamsiboyella <vamsikrishnaboyella@gmail.com>
2020-04-04 15:43:32 -0400

    fixed minor structural changes to create multiple branches

--- src/admin.h
+++
@@ -1,6 +0,0 @@
-void insertfirst(int data, char foodname[25], int quantity, float price);
-void insertmid(int pos, int data, char foodname[25], int quantity, float price);
-void insertend(int data, char foodname[25], int quantity, float price);
-void updatefood(int udata, int uquantity);
-void foodlist();
-void deletefood(int serial);
